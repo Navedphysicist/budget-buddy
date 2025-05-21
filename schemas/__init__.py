c10621b9b47c379c1d17e041fed1fe3ac7baae58
Naved Khan <navedphsysicist@gmail.com>
2025-05-21 15:27:08 +0530

    Update with CategoryCreate and the PaymentModeCreate error

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,13 +1,13 @@
-from schemas.category import Category, CategoryCreate, CategoryBase, CategoryWithExpense
+from schemas.category import Category, CategoryBase, CategoryWithExpense
 from schemas.expense import Expense, ExpenseCreate, ExpenseBase, ExpenseUpdate
-from schemas.payment_mode import PaymentMode, PaymentModeCreate, PaymentModeBase
+from schemas.payment_mode import PaymentMode, PaymentModeBase
 from schemas.income import Income, IncomeCreate, IncomeBase, IncomeUpdate
 from schemas.testimonial import Testimonial, TestimonialCreate, TestimonialBase
 
 __all__ = [
-    'Category', 'CategoryCreate', 'CategoryBase', 'CategoryWithExpense',
+    'Category', 'CategoryBase', 'CategoryWithExpense',
     'Expense', 'ExpenseCreate', 'ExpenseBase', 'ExpenseUpdate',
-    'PaymentMode', 'PaymentModeCreate', 'PaymentModeBase',
+    'PaymentMode', 'PaymentModeBase',
     'Income', 'IncomeCreate', 'IncomeBase', 'IncomeUpdate',
     'Testimonial', 'TestimonialCreate', 'TestimonialBase'
 ]
